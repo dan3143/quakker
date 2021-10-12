9d12f2246bc190e52b4d4ed82bb43cb480d13bd1
Víctor Daniel Guevara <vdguevara@uninorte.edu.co>
2021-10-12 12:57:33 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.join(__dirname, "src", "index.tsx"),
+  output: { path: path.join(__dirname, "build"), filename: "index.bundle.js" },
+  mode: process.env.NODE_ENV || "development",
+  resolve: { modules: [path.resolve(__dirname, "src"), "node_modules"] },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ["ts-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|jp(e?)g|gif)/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+      },
+    ],
+  },
+  devServer: {
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html"),
+    }),
+  ],
+};
