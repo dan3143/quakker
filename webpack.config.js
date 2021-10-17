03b8459e7ea53b0c1575fc2d977b1f64e0f3a45e
Víctor Daniel Guevara <vdguevara@uninorte.edu.co>
2021-10-17 10:21:28 -0500

    Formatting

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,11 @@ const webpack = require("webpack");
 
 module.exports = {
   entry: path.join(__dirname, "src", "index.tsx"),
-  output: { path: path.join(__dirname, "build"), filename: "index.bundle.js" },
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: "index.bundle.js",
+    publicPath: "/",
+  },
   mode: process.env.NODE_ENV || "development",
   resolve: {
     extensions: [".ts", ".tsx", ".json", ".js", ".jsx"],
@@ -52,6 +56,7 @@ module.exports = {
     ],
   },
   devServer: {
+    historyApiFallback: true,
     hot: true,
   },
   plugins: [
